16ff6f9f44d9ca9b8a3ece450e1f65f84b827c3c
Derek Hammitt <derek.hammitt@shuksan.local>
2023-06-06 21:30:15 -0500

    install doomemacs

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,15 @@ cat <<-EOF > ~/.zshrc
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 EOF
 ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications/Emacs.app
+
+DOOMDIR=~/.config/emacs
+if [ ! -d "$DOOMDIR" ]; then
+	git clone --depth 1 https://github.com/doomemacs/doomemacs "$DOOMDIR"
+    "$DOOMDIR"/bin/doom install
+else
+  "$DOOMDIR"/bin/doom upgrade
+fi
+
+if [ -d ~/.emacs.d ]; then
+  rm -rf ~/.emacs.d
+fi
