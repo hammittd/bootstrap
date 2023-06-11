506c9e9856e5163123e6effff7fb0037971abbd8
Derek Hammitt <derek@olympia.local>
2023-06-11 17:06:01 -0500

    key repeat stuff is faster

--- bootstrap.sh
+++ bootstrap.sh
@@ -49,3 +49,7 @@ ZSHRC="$HOME/.zshrc"
 
 [ -z $(grep "$BELL_SETTING" "$ZSHRC") ] && echo "$BELL_SETTING" >> "$ZSHRC"
 
+# Set initial key repeat to 10 (150ms)
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+# Setting key repeat to 1 (15ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
