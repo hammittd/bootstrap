5e4f627611a118ea1afbf8b9fe20c6e3dc708457
derek <hammittd@icloud.com>
2023-09-28 17:14:40 -0500

    fix indents, do not overwrite .zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
+
 echo 'Updating software with softwareupdate --install --recommended'
 softwareupdate --install --recommended
 
 if ! command -v brew &> /dev/null
 then
-    /bin/bash -c \
-        "$(curl -fsSL \
-        https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-    echo 'homebrew already installed. Running brew bundle...'
+  echo 'homebrew already installed. Running brew bundle --file ./.Brewfile'
 fi
 
 brew bundle --file ./.Brewfile
@@ -16,11 +16,6 @@ brew bundle --file ./.Brewfile
 if [ ! -d ~/.fzf.zsh ]; then
   /usr/local/opt/fzf/install --all
 fi
-
-cat <<-EOF > ~/.zshrc
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-EOF
-
 
 GIT_USER=$(git config user.name)
 if [ -z "$GIT_USER" ]; then
