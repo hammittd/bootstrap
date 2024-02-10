cb54f32303337f0f8dcc6c06fc3febd233c4e49f
derek <derekrhammitt@gmail.com>
2024-02-10 15:04:59 -0600

    Add gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,3 +48,64 @@ if confirm_step "Would you like to configure git?"; then
     git config --global user.name "$git_username"
     git config --global user.email "$git_email"
   fi
+
+  echo "The following git configuration will be applied:"
+  echo "git config --global init.defaultBranch main"
+  echo "git config --global core.editor "code --wait""
+  echo "git config --global alias.co checkout"
+  echo "git config --global alias.br branch"
+  echo "git config --global alias.ci commit"
+  echo "git config --global alias.st status"
+  echo "git config --global alias.sw switch"
+  echo "git config --global help.autocorrect 1"
+  echo "git config --global push.default current"
+  echo "git config --global credential.helper osxkeychain"
+  echo "git config --global pull.rebase true"
+
+  if confirm_step "Would you like to apply these settings?"; then
+    git config --global init.defaultBranch main
+    git config --global core.editor "code --wait"
+    git config --global alias.co checkout
+    git config --global alias.br branch
+    git config --global alias.ci commit
+    git config --global alias.st status
+    git config --global alias.sw switch
+    git config --global help.autocorrect 1
+    git config --global push.default current
+    git config --global credential.helper osxkeychain
+    git config --global pull.rebase true
+  fi
+
+  echo "Do you want to sign your Git commits? Choose an option:"
+  echo "1) GPG"
+  echo "2) SSH"
+  echo "3) No signing"
+  read -rp "Enter choice (1/2/3): " signing_choice
+
+  case $signing_choice in
+    1)
+      read -rp "Enter GPG key ID: " gpg_key_id
+      if [[ -n "$gpg_key_id" ]]; then
+        git config --global user.signingkey "$gpg_key_id"
+        git config --global commit.gpgsign true
+        echo "Git commits will be signed with GPG."
+      else
+        echo "GPG key ID not provided. Skipping GPG signing configuration."
+      fi
+      ;;
+    2)
+      read -rp "Enter SSH key key ID (if you're using 1Password, copy/paste the public key from the SSH key in 1password): " ssh_key_id
+      read -rp "Enter path to ssh program (if you're using 1Password, this is probably /Applications/1Password.app/Contents/MacOS/op-ssh-sign): " ssh_program
+      git config --global user.signingkey "$ssh_key_id"
+      git config --global gpg.format ssh
+      git config --global gpg.ssh.program "$ssh_program"
+      git config --global commit.gpgsign true
+      ;;
+    3)
+      echo "Git commits will not be signed."
+      ;;
+    *)
+      echo "Invalid choice. Skipping commit signing configuration."
+      ;;
+  esac
+fi
