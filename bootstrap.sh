35f5a3d58b812eda87fb4c6fdb2125a6c177749b
derek <hammittd@icloud.com>
2024-02-10 13:56:42 -0600

    Add confirm_step

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+confirm_step() {
+    while true; do
+        read -p "$1 (Y/n) " yn
+        case ${yn:-Y} in
+            [Yy]* | "" ) break;; # Treat empty input as 'yes' and break from the loop
+            [Nn]* ) echo "Skipping step."; return 1;; # If no, return 1 to indicate to skip the step
+            * ) echo "Please answer yes (Y) or no (n).";;
+        esac
+    done
+}
+
 echo 'Updating software with softwareupdate --install --recommended'
 softwareupdate --install --recommended
 
