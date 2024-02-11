a00949a9aed24395e2b818e8afb1ec8844beeeff
derek <derekrhammitt@gmail.com>
2024-02-10 19:18:05 -0600

    silence shellcheck warning

--- bootstrap.sh
+++ bootstrap.sh
@@ -219,6 +219,7 @@ fi
 echo ""
 if confirm_step "Install nvm and the latest LTS version of NodeJS?"; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  # shellcheck source=/dev/null
   source "$ZSHRC"
   nvm install --lts
   nvm use --lts
